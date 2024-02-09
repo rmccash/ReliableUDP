335161eb19a61fe63cb87cfe25cddb62a19de518
rmccash <rmccash5169@conestogac.on.ca>
2024-02-09 18:44:03 -0500

    Created functions for calculating crc-32  and verifying the file integrity of the files sent and received

--- FileVerification.h
+++ FileVerification.h
@@ -1,1 +1,21 @@
+
+//Filename:		FileVerification.h
+//Assignment:	A-01
+//Author:		Rhys McCash & Andrew Babos
+//Student #:	8825169 & 8822549
+//Date:			2/7/2024
+//Description:	Contains the function prototypes for the functions in FileVerification.cpp
+
 #pragma once
+
+#include <string>
+#include <cstdint>
+
+using namespace std;
+
+namespace file_verification 
+{
+    uint32_t calculateCRC32(const char* buffer, size_t length);
+
+    bool verifyFileIntegrity(const string& filePath);
+} 
