21a97e9d2bae4ab03ee5820bfb9f1733afdf8a61
Andrew3369 <andrewjanosbabos@gmail.com>
2024-02-10 17:05:41 -0500

    Added in the RecieveFile method. Takes the bytes from the packet and writes it to a file. Also changed variables for better readbility

--- FileTransfer.cpp
+++ FileTransfer.cpp
@@ -1,4 +1,3 @@
-
 #include "FileTransfer.h"
 #include "Net.h"
 #include <iostream>
@@ -8,7 +7,7 @@
 using namespace std;
 
 FileTransfer::FileTransfer(string filename) : net::ReliableConnection(protocolId, timeout)
-{ 
+{
     this->filename = filename;
 }
 
@@ -19,7 +18,7 @@ FileTransfer::~FileTransfer()
 
 void FileTransfer::SendFile(const char* filename, const char* address, int port)
 {
-    // create holder
+    // open binary file
     ifstream file(filename, ios::binary);
 
     // if no file was found
@@ -29,18 +28,18 @@ void FileTransfer::SendFile(const char* filename, const char* address, int port)
         return;
     }
 
-    char buffer[PacketSizeHack]; // temp holder
+    char packet[PacketSizeHack]; // temp holder
 
     // Read each byte and send it to the client
     while (!file.eof())
     {
         // Reads up to the packetsizehack from earlier labs, and then sends that
-        file.read(buffer, PacketSizeHack);
+        file.read(packet, PacketSizeHack);
         int bytesRead = file.gcount();
         if (bytesRead > 0)
         {
-            // Send the packet
-            SendPacket((unsigned char*)buffer, bytesRead);
+            // Send the packet (data in binary)
+            SendPacket((unsigned char*)packet, bytesRead);
         }
     }
 
@@ -49,5 +48,34 @@ void FileTransfer::SendFile(const char* filename, const char* address, int port)
 
 void FileTransfer::ReceiveFile(const char* filename, int port)
 {
+	// create holder
+	ofstream file(filename, ios::binary);
+
+	// if no file was found
+	if (!file)
+	{
+		cerr << "Error opening file" << endl;
+		return;
+	}
+
+	char buffer[PacketSizeHack]; // temp holder
+
+	// Read each byte and send it to the client
+	while (true)
+	{
+		// Receive the packet
+		int bytesRead = ReceivePacket((unsigned char*)buffer, PacketSizeHack);
+		if (bytesRead > 0)
+		{
+			// Write the packet to the file
+			file.write(buffer, bytesRead);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	file.close();
 
 }
